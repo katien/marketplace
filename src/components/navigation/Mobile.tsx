e177664b7c99e05c92add4a1342eef6bc7a2e96c
Breads <breads.@Breadss-MacBook-Pro.local>
2022-07-05 02:29:13 -0600

    hide wallet connect on mobile view until portis cookie issue can be solved

--- src/components/navigation/Mobile.tsx
+++ src/components/navigation/Mobile.tsx
@@ -79,9 +79,6 @@ function Navigation() {
                 </a>
               ))}
             </div>
-            <div>
-              <ConnectWallet classes="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700" />
-            </div>
           </div>
         </div>
       </Popover.Panel>
